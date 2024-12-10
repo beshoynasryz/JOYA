8be255c9c49a76d2efbe8f881cc714ac8dc37afc
beshoynasryz <beshoynasry1@gmail.com>
2024-12-10 06:45:23 +0200

    property

--- src/component/Home/Presentation.jsx
+++ src/component/Home/Presentation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image5 from "../../images/home/home.jpg";
+import image5 from "../../images/sliders/1920x1080.jpg";
 
 const Presentation = () => {
   //no problem
@@ -26,7 +26,7 @@ const Presentation = () => {
           <div className="w-full lg:w-1/2 px-4">
             <div className="relative group">
               <img
-                src='../../images/home/home.jpg'
+                src={image5}
                 alt="joya properties"
                 className="rounded-lg shadow-lg transition-transform transform group-hover:scale-105 w-full"
                 style={{ maxHeight: '855px' }}
